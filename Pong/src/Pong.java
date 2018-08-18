adea7b6aea0b929322cce65a1d1517f9ee886b61
simongreen-op <simongreen3@myopenpolytechnic.ac.nz>
2018-08-18 17:16:25 +1200

    Use constants for values

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -1,9 +1,21 @@
+import javax.swing.JFrame;
+ 
+public class Pong extends JFrame {
 
-public class Pong {
+	private final static int WINDOW_WIDTH = 800;
+	private final static int WINDOW_HEIGHT = 600;
+	private final static String WINDOW_TITLE = "Pong";
+	
+	public Pong() {
+    	setTitle(WINDOW_TITLE);
+    	setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    	setResizable(false);
+    	setVisible(true);
+    	setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new Pong();
 	}
 
 }
