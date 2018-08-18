c060e3682198a1472bd0541165708cc3db2c53f5
simongreen-op <simongreen3@myopenpolytechnic.ac.nz>
2018-08-18 17:49:41 +1200

    A dotted line :)

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -3,14 +3,36 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
+import java.awt.BasicStroke;
 import javax.swing.JPanel;
+import javax.swing.Timer;
   
 public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	private final static Color BACKGROUND_COLOR = Color.BLACK;
+	private final static int TIMER_DELAY = 5;
+    
 	public PongPanel() {
-        setBackground(BACKGROUND_COLOR);
-    }
-	 
+	    setBackground(BACKGROUND_COLOR);
+	    Timer timer = new Timer(TIMER_DELAY, this);
+	    timer.start();
+	}
+
+	private void update() {
+        
+	}
+
+	private void paintDottedLine(Graphics g) {
+	    Graphics2D g2d = (Graphics2D) g.create();
+	    Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
+	    g2d.setStroke(dashed);
+	    g2d.setPaint(Color.WHITE);
+	    g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
+	    g2d.dispose();
+	}
+
 	@Override
 	public void keyPressed(KeyEvent event) {
 		// TODO Auto-generated method stub
@@ -31,8 +53,14 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
-		// TODO Auto-generated method stub
+		update();
+		repaint();
 		
 	}
-  
+ 
+	@Override
+	public void paintComponent(Graphics g) {
+	    super.paintComponent(g);
+	    paintDottedLine(g);
+	}
  }
